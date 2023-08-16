28e480a360256d95f9a88941fb9728d5bbbbe791
Alex Vest <alex.vest@nutmeg.com>
2023-08-16 13:52:26 +0100

    Finish snippets
    Could not get separation into another file owrking

--- lua/avestuk/lsp.lua
+++ lua/avestuk/lsp.lua
@@ -105,6 +105,15 @@ local filename = function()
     return { vim.fn.expand "%:p" }
 end
 
+luasnip.config.set_config {
+    history = true,
+    -- treesitter-hl has 100, use something higher (default is 200).
+    ext_base_prio = 200,
+    -- minimal increase in priority.
+    ext_prio_increase = 1,
+    enable_autosnippets = false,
+    store_selection_keys = "<c-s>",
+}
 
 luasnip.add_snippets(nil, {
     all = {
--- lua/avestuk/snippets.lua
+++ lua/avestuk/snippets.lua
@@ -1,18 +1,4 @@
-local ls = require "luasnip"
-local fmt = require("luasnip.extras.fmt").fmt
-local rep = require("luasnip.extras").rep
 
-
-
-ls.config.set_config {
-    history = true,
-    -- treesitter-hl has 100, use something higher (default is 200).
-    ext_base_prio = 200,
-    -- minimal increase in priority.
-    ext_prio_increase = 1,
-    enable_autosnippets = false,
-    store_selection_keys = "<c-s>",
-}
 
 
 
