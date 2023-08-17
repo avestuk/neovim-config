2941cbe3277562914903220d111218018c2097fd
Alex Vest <alex.vest@nutmeg.com>
2023-08-17 11:13:53 +0100

    Rebind error list

--- lua/avestuk/treesitter.lua
+++ lua/avestuk/treesitter.lua
@@ -1,72 +1,72 @@
 local treesitter = require('nvim-treesitter.configs')
 
 treesitter.setup {
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = 'ga',
-      node_incremental = 'ga',
-      node_decremental = 'gz',
+    ensure_installed = {
+                        "bash",
+                        "dockerfile",
+                        "go",
+                        "hcl",
+                        "json",
+                        "lua",
+                        "markdown",
+                        "python",
+                        "rust",
+                        "yaml"
+                        },
+    highlight = {
+        enable = true
     },
-  },
-  textobjects = {
-    select = {
+    incremental_selection = {
       enable = true,
-      lookahead = true,
       keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-        ['ia'] = '@parameter.inner',
-      }
-    },
-    swap = {
-      enable = true,
-      swap_previous = {
-        ['{a'] = '@parameter.inner',
-      },
-      swap_next = {
-        ['}a'] = '@parameter.inner',
+        init_selection = 'ga',
+        node_incremental = 'ga',
+        node_decremental = 'gz',
       },
     },
-    move = {
-      enable = true,
-      set_jumps = true,
-      goto_next_start = {
-        [']f'] = '@function.outer',
-        [']c'] = '@class.outer',
-        [']a'] = '@parameter.inner',
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+          ['ia'] = '@parameter.inner',
+        }
       },
-      goto_next_end = {
-        [']F'] = '@function.outer',
-        [']C'] = '@class.outer',
+      swap = {
+        enable = true,
+        swap_previous = {
+          ['{a'] = '@parameter.inner',
+        },
+        swap_next = {
+          ['}a'] = '@parameter.inner',
+        },
       },
-      goto_previous_start = {
-        ['[f'] = '@function.outer',
-        ['[c'] = '@class.outer',
-        ['[a'] = '@parameter.inner',
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          [']f'] = '@function.outer',
+          [']c'] = '@class.outer',
+          [']a'] = '@parameter.inner',
+        },
+        goto_next_end = {
+          [']F'] = '@function.outer',
+          [']C'] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[f'] = '@function.outer',
+          ['[c'] = '@class.outer',
+          ['[a'] = '@parameter.inner',
+        },
+        goto_previous_end = {
+          ['[F'] = '@function.outer',
+          ['[C'] = '@class.outer',
+        },
       },
-      goto_previous_end = {
-        ['[F'] = '@function.outer',
-        ['[C'] = '@class.outer',
-      },
-    },
-  },
-  ensure_installed = {
-                      "bash",
-                      "dockerfile",
-                      "go",
-                      "hcl",
-                      "json",
-                      "lua",
-                      "markdown",
-                      "python",
-                      "rust",
-                      "yaml"
-                      },
-  highlight = {
-      enable = true
-  }
+    }
 }
 
